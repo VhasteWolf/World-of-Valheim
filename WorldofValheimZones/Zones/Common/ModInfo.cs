f6d679b37a5e0a3a2da8184e34b7c59d0b6df7ec
VhasteWolf <vhastewolf@gmail.com>
2022-09-03 20:10:50 -0300

    add references

--- WorldofValheimZones/Zones/Common/ModInfo.cs
+++ WorldofValheimZones/Zones/Common/ModInfo.cs
@@ -11,7 +11,7 @@ namespace WorldofValheimZones
         public const string Group = "HackShardGaming";
         public const string Guid = "HackShardGaming.WorldofValheimZones";
         // Version follow Semantic Versioning Scheme (https://semver.org/)
-        public const string Version = "0.8.2";
+        public const string Version = "0.8.3";
         public const string buildDate = "2022-09-03";
         // Nexus Plugin ID (Use to maintain updates with Nexus)
         public const int NexusID = 891;
